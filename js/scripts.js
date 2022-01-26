8913f6f1a2f8342af2bc4a6d59f66f008c1c56cd
rommel-av <98291987+rommel-av@users.noreply.github.com>
2022-01-25 20:21:18 -0600

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,69 @@
+/*!
+* Start Bootstrap - Grayscale v7.0.4 (https://startbootstrap.com/theme/grayscale)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+ScrollReveal().reveal('.text-white', { delay:250 });
+ScrollReveal().reveal('.text-white-50', { delay:350 });
+ScrollReveal().reveal('.descripcion', { delay:300 });
+ScrollReveal().reveal('.text-black-50', { delay:450 });
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+ 
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+window.addEventListener('online', actualizarStatusOnline)
+window.addEventListener('offline', actualizarStatusOnline)
+
+function actualizarStatusOnline(event) {
+    const condition = navigator.onLine
+    ? 'online'
+    : 'offline'
+
+    console.log(`El usuario esta ${condition}`);
+}
